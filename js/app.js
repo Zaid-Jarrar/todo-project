28531666296f226993fefc2e710cb094a7434ad7
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-01-22 14:19:27 +0200

    fixing js

--- js/app.js
+++ js/app.js
@@ -1,39 +1,42 @@
-`use strict`;
+/* eslint-disable no-redeclare */
 
-var name = prompt("Please enter your name");
+'use strict';
 
-var gender = prompt("Please enter your gender (Male/Female)").toUpperCase();
+var name = prompt('Please enter your name');
 
-if (gender !== "MALE" && gender !== "FEMALE") {
+var gender = prompt('Please enter your gender (Male/Female)').toUpperCase();
 
-    alert("Please enter either Male or Female")
-    var gender = prompt("Please enter your gender (Male/Female)").toUpperCase();
+if (gender !== 'MALE' && gender !== 'FEMALE') {
+
+  alert('Please enter either Male or Female');
+
+  var gender = prompt('Please enter your gender (Male/Female)').toUpperCase();
 }
 
-var age = prompt("Please enter your age")
+var age = prompt('Please enter your age');
 if (age <= 0) {
-
-    alert("Please enter your age")
-    var age = prompt("Please enter your age")
+  alert('Please enter your age');
+  var age = prompt('Please enter your age');
 
 }
 
 
 
-var message = prompt("Would you like to skip the welcoming Message?").toUpperCase()
-if (message === "YES") {
-    var confirmation = confirm("Are you sure?")
+var message = prompt('Would you like to skip the welcoming Message?').toUpperCase();
+if (message === 'YES') {
+  // eslint-disable-next-line no-unused-vars
+  var confirmation = confirm('Are you sure?');
 }
 else {
-    if (gender === "MALE") {
-        alert(`welcome to Mr. ${name} the website`)
-    }
-    else if (gender === "FEMALE") {
-        alert(`welcome to Mrs. ${name} the website`)
-    }
-    else {
-        alert(`welcome ${name} to the website`)
-    }
+  if (gender === 'MALE') {
+    alert(`welcome to Mr. ${name} the website`);
+  }
+  else if (gender === 'FEMALE') {
+    alert(`welcome to Mrs. ${name} the website`);
+  }
+  else {
+    alert(`welcome ${name} to the website`);
+  }
 
 }
 
